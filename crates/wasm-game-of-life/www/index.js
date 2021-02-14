9a8a8ecb850ea0c8c261e61360c2a2239a624e5a
yuchanns <airamusume@gmail.com>
2021-02-14 21:35:20 +0800

    chore: improve game-of-life

--- crates/wasm-game-of-life/www/index.js
+++ crates/wasm-game-of-life/www/index.js
@@ -104,13 +104,32 @@ const drawCells = () => {
 
     ctx.beginPath();
 
+    // Alive cells.
+    ctx.fillStyle = ALIVE_COLOR;
     for (let row = 0; row < height; row++) {
         for (let col = 0; col < width; col++) {
             const idx = getIndex(row, col);
+            if (cells[idx] !== Cell.Alive) {
+                continue;
+            }
 
-            ctx.fillStyle = cells[idx] === Cell.Dead
-                ? DEAD_COLOR
-                : ALIVE_COLOR;
+            ctx.fillRect(
+                col * (CELL_SIZE + 1) + 1,
+                row * (CELL_SIZE + 1) + 1,
+                CELL_SIZE,
+                CELL_SIZE
+            );
+        }
+    }
+
+    // Dead cells.
+    ctx.fillStyle = DEAD_COLOR;
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            const idx = getIndex(row, col);
+            if (cells[idx] !== Cell.Dead) {
+                continue;
+            }
 
             ctx.fillRect(
                 col * (CELL_SIZE + 1) + 1,
