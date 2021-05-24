d3443452c3093984f5f6c223a33a16e49f0a2e49
yuchanns <airamusume@gmail.com>
2021-05-24 23:51:25 +0800

    writing-os: add paging impl

--- writing-an-os-in-rust/src/lib.rs
+++ writing-an-os-in-rust/src/lib.rs
@@ -7,10 +7,14 @@
 
 pub mod gdt;
 pub mod interrupts;
+pub mod memory;
 pub mod serial;
 pub mod vga_buffer;
 
 use core::panic::PanicInfo;
+
+#[cfg(test)]
+use bootloader::{entry_point, BootInfo};
 
 pub fn init() {
     gdt::init();
@@ -72,8 +76,10 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
 }
 
 #[cfg(test)]
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+entry_point!(test_kernel_main);
+
+#[cfg(test)]
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
     init();
     test_main();
     hlt_loop();
--- writing-an-os-in-rust/src/main.rs
+++ writing-an-os-in-rust/src/main.rs
@@ -4,8 +4,12 @@
 #![test_runner(writing_an_os_in_rust::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use writing_an_os_in_rust::println;
+use writing_an_os_in_rust::memory::{active_level_4_table, BootInfoFrameAllocator};
+use writing_an_os_in_rust::{memory, println};
+use x86_64::structures::paging::{Page, PageTable, Translate};
+use x86_64::VirtAddr;
 
 #[cfg(not(test))]
 #[panic_handler]
@@ -20,8 +24,9 @@ fn panic(info: &PanicInfo) -> ! {
     writing_an_os_in_rust::test_panic_handler(info)
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
     println!("Hello Yuchanns{}", "!");
 
     writing_an_os_in_rust::init();
