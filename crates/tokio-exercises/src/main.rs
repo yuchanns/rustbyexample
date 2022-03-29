619705643bbae874fd983d72460904581ab48256
yuchanns <me@yuchanns.xyz>
2022-03-29 22:24:51 +0800

    add tokio-exercises

---
+++ crates/tokio-exercises/src/main.rs
@@ -0,0 +1,42 @@
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpListener,
+};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("0.0.0.0:8080").await?;
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    Ok(n) if n == 0 => return,
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
+                let s = match std::str::from_utf8(&buf[0..n]) {
+                    Ok(v) => v,
+                    Err(_) => "",
+                };
+
+                if s == "quit\n" {
+                    println!("receive quit");
+                    return;
+                }
+
+                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                    eprintln!("failed to write to socket; err = {:?}", e);
+                    return;
+                }
+            }
+        });
+    }
+}
